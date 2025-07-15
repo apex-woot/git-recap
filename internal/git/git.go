443c7800173a078f43c6c4ea7625511ab3ec157c
Vitalii Kohut <vkohut@intelliarts.com>
2025-07-15 14:14:04 +0300

    feat: fetch commits from git

--- internal/git/git.go
+++ internal/git/git.go
@@ -5,16 +5,36 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 )
 
-func GetCommits() []string {
-	cmd := exec.Command("git", "log", "--oneline")
+type GitCommit struct {
+	ShortHash string
+	Msg       string
+	Author    string
+	Date      time.Time
+}
+
+func GetCommits(since time.Time) ([]*GitCommit, error) {
+	cmd := exec.Command("git", "log", "--pretty=format:%h|%s|%an|%ad", "--date=iso", fmt.Sprintf("--since=%s", since.Format("2006-01-02")))
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to execute git log command: %v", err)
+		return nil, fmt.Errorf("failed to execute git log command: %v", err)
+	}
+	commits := strings.FieldsFunc(string(output), func(r rune) bool { return r == '\n' })
+	gitCommits := make([]*GitCommit, len(commits))
+	for i, c := range commits {
+		gitCommitParts := strings.FieldsFunc(strings.TrimSpace(c), func(r rune) bool { return r == '|' })
+		timestamp, err := time.Parse("2006-01-02 15:04:05 -0700", gitCommitParts[3])
+		if err != nil {
+			return nil, fmt.Errorf("can not parse commit date: %w", err)
+		}
+		gitCommit := GitCommit{ShortHash: gitCommitParts[0], Msg: gitCommitParts[1], Author: gitCommitParts[2], Date: timestamp}
+		gitCommits[i] = &gitCommit
 	}
 
-	commits := strings.Split(string(output), "\n")
-	fmt.Println(commits)
-	return commits
+	for _, c := range gitCommits {
+		fmt.Fprintf(os.Stderr, "%+v\n", *c)
+	}
+	return gitCommits, nil
 }
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/apex-woot/git-recap/internal/flags"
+	"github.com/apex-woot/git-recap/internal/git"
 )
 
 func main() {
@@ -12,8 +14,12 @@ func main() {
 	fmt.Printf("%+v\n\n", cfg)
 
 	// Check if the folder is a root repo folder (has .git) folder
-	//
+
 	// Get commit history using git api
+	_, err := git.GetCommits(cfg.Since)
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Parse the history and map days-to-group of commits done in that day
 	// Output the history to the io.Writer
 }
